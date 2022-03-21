43a9a94b47984145ea540fa4aaf3935648778d58
CamilleRungette <c.rungette@gmail.com>
2022-03-21 14:44:12 +0100

    home page

--- src/content/Home/Home.js
+++ src/content/Home/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { CarouselData } from './Carousel/CarouselData';
-import {Carousel, Lines1} from "./_index";
+import {Carousel, Lines1, Lines2, Lines3} from "./_index";
 
 const Home = () => {
 
   return (
     <div className='home-main'>
-      <Carousel slides={CarouselData} />
-      <Lines1/>
+      <div className='carousel-div'>
+        <Carousel slides={CarouselData} />
+      </div>
+
+      <div className='home-lines1'>
+        <Lines1 />
+      </div>
+
+      <div className='quote'>
+        <p>Hoc Momento est une compagnie internationale et pluridisciplinaire qui désire dépasser la séparation entre théâtre d'art et théâtre militant</p>
+      </div>
+
+      <div className='home-lines2'>
+        <Lines2/>
+      </div>
     </div>
   )
 }; 
--- src/content/Home/_index.js
+++ src/content/Home/_index.js
@@ -1,2 +1,4 @@
 export {default as Carousel} from "./Carousel/Carousel";
 export {default as Lines1} from "../Lines/Lines1";
+export {default as Lines2} from "../Lines/Lines2";
+export {default as Lines3} from "../Lines/Lines3";
