d76e2ebf78b120ebce450f05da574df7f34284c9
CamilleRungette <c.rungette@gmail.com>
2022-04-01 11:48:00 +0200

    test

--- src/content/Navbar/NavbarHome.js
+++ src/content/Navbar/NavbarHome.js
@@ -13,7 +13,7 @@ const NavbarHome = () => {
       changeBackground();
     window.addEventListener("scroll", changeBackground);
 
-    axios.get(`${process.env.REACT_APP_URL}/shows`)
+    axios.get(`https://hoc-momento-back.herokuapp.com/shows`)
     .then(res => {
       console.log("response", res);
       let content = <ul className='no-list-style menu-list'>
