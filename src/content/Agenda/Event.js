0958ecc9b4374613d8c7779b3a0a439b979faece
CamilleRungette <c.rungette@gmail.com>
2022-05-02 09:02:56 +0200

    changed show to dates in old db

--- src/content/Agenda/Event.js
+++ src/content/Agenda/Event.js
@@ -14,24 +14,24 @@ const Event = ({event, date}) => {
   return (
     <div className='event-div'>
       <button className='primary-button-outline see-more' onClick={showModal}> Voir </button>
-      <h2 className='event-month'>{months[new Date(event.show[0].startDate).getMonth() +1]} {year} </h2>
+      <h2 className='event-month'>{months[new Date(event.dates[0].startDate).getMonth() +1]} {year} </h2>
       <p>
-        {new Date(event.show[0].startDate).getDate() === new Date(event.show[0].endDate).getDate() ?
-          <span>Le {new Date(event.show[0].startDate).getDate()} {months[new Date(event.show[0].startDate).getMonth() +1]} </span>
+        {new Date(event.dates[0].startDate).getDate() === new Date(event.dates[0].endDate).getDate() ?
+          <span>Le {new Date(event.dates[0].startDate).getDate()} {months[new Date(event.dates[0].startDate).getMonth() +1]} </span>
         : 
         <span>
-          Du {new Date(event.show[0].startDate).getDate()} 
-            {new Date(event.show[0].startDate).getMonth() !== new Date(event.show[0].endDate).getMonth() ? (
-            <span> {months[new Date(event.show[0].startDate).getMonth() +1]} </span>
+          Du {new Date(event.dates[0].startDate).getDate()} 
+            {new Date(event.dates[0].startDate).getMonth() !== new Date(event.dates[0].endDate).getMonth() ? (
+            <span> {months[new Date(event.dates[0].startDate).getMonth() +1]} </span>
             ):( <> </>)}
-          au {new Date(event.show[0].endDate).getDate()} {months[new Date(event.show[0].endDate).getMonth() +1]}
+          au {new Date(event.dates[0].endDate).getDate()} {months[new Date(event.dates[0].endDate).getMonth() +1]}
         </span>}
       </p>
 
       <h2 className='event-name'>{event.title}</h2> 
 
-      {event.show[0].place && <p>{event.show[0].place} </p> }
-      <p>{event.show[0].address ? <span>{event.show[0].address},</span> : <></>} {event.show[0].city ? event.show[0].city : <></>}</p>
+      {event.dates[0].place && <p>{event.dates[0].place} </p> }
+      <p>{event.dates[0].address ? <span>{event.dates[0].address},</span> : <></>} {event.dates[0].city ? event.dates[0].city : <></>}</p>
       <BasicModal ref={modalRef} content={
         <div className='event-description'>
           <div className='picture'>
