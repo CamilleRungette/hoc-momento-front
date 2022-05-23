1a7da00c358d19414d093530fc1fbfe5b319da94
CamilleRungette <c.rungette@gmail.com>
2022-05-23 15:45:12 +0200

    limited the display of 'Test' data

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-// const url = process.env.REACT_APP_BACK_END
-const url = "http://localhost:3000";
+const url = process.env.REACT_APP_BACK_END;
+// const url = "http://localhost:3000";
 
 export default url;
