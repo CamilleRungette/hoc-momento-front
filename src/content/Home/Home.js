aee2fe3310a38d4c83497960573f167a458ee854
CamilleRungette <c.rungette@gmail.com>
2022-07-27 21:04:33 +0200

    test

--- src/content/Home/Home.js
+++ src/content/Home/Home.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { CarouselData } from './Carousel/CarouselData';
-import {Carousel, Lines1, Lines2, HomeFooter, NavbarHome} from "./_index";
-
+import React from "react";
+import { CarouselData } from "./Carousel/CarouselData";
+import { Carousel, Lines1, Lines2, HomeFooter, NavbarHome } from "./_index";
 
 const Home = () => {
-
   return (
-    <div className='home-main'>
+    <div className="home-main">
       <NavbarHome />
-      <div className='carousel-home-div'>
+      <div className="carousel-home-div">
         <Carousel gallery={CarouselData} />
       </div>
 
-      <div className='home-lines1'>
+      <div className="home-lines1">
         <Lines1 />
       </div>
 
-      <div className='quote'>
-        <p>Hoc Momento est une compagnie internationale et pluridisciplinaire qui désire dépasser la séparation entre théâtre d'art et théâtre militant</p>
+      <div className="quote">
+        <p>
+          Hoc Momento est une compagnie internationale et pluridisciplinaire qui
+          désire dépasser la séparation entre théâtre d'art et théâtre militant.
+        </p>
       </div>
 
-      <div className='home-lines2'>
-        <Lines2/>
+      <div className="home-lines2">
+        <Lines2 />
       </div>
 
-      <HomeFooter/>
+      <HomeFooter />
     </div>
-  )
-}; 
+  );
+};
 
 export default Home;
